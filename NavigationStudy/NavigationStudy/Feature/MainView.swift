2fc87a9fd2e2cc653911e3b6ef0b1abeba168806
kdjun97 <kdjun97@gmail.com>
2024-01-27 22:50:37 +0900

    feat/navigation: add bottom sheet

--- NavigationStudy/NavigationStudy/Feature/MainView.swift
+++ NavigationStudy/NavigationStudy/Feature/MainView.swift
@@ -19,14 +19,71 @@ struct MainView: View {
     }
     
     var body: some View {
-        Text("Main View")
-        Button {
-            viewStore.send(.navigateToDetailButtonTapped)
-        } label: {
-            Text("Navigate to Detail")
-                .foregroundStyle(.white)
-                .padding()
-                .background(.black)
+        MainContentView(store: store)
+            .sheet(isPresented: viewStore.binding(
+                    get: \.isSheetPresented,
+                    send: { newValue in .setSheetPresented(newValue)})
+            ) {
+                MainBottomSheetView(store: store)
+                    .presentationDetents([
+                        .medium
+                    ]).presentationDragIndicator(.visible)
+            }
+    }
+}
+
+private struct MainContentView: View {
+    let store: StoreOf<MainFeature>
+    
+    init(store: StoreOf<MainFeature>) {
+        self.store = store
+    }
+    
+    fileprivate var body: some View {
+        VStack {
+            Text("Main View")
+            Button {
+                store.send(.navigateToDetailButtonTapped)
+            } label: {
+                Text("Navigate to Detail")
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.black)
+            }
+            Button {
+                store.send(.showSheetButtonTapped)
+            } label: {
+                Text("Show Sheet")
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.black)
+            }
+        }
+    }
+}
+
+private struct MainBottomSheetView: View {
+    let store: StoreOf<MainFeature>
+    
+    init(store: StoreOf<MainFeature>) {
+        self.store = store
+    }
+    
+    fileprivate var body: some View {
+        VStack {
+            Text("내용 대충")
+                .padding(.top, 48)
+            Spacer()
+            Button {
+                store.send(.bottomSheetSendButtonTapped)
+            } label: {
+                Text("Send")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(.orange)
+                    .padding(.bottom, 24)
+                    .padding(.horizontal, 12)
+            }
         }
     }
 }
